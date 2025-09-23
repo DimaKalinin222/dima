43e4c46af273f09af9040f8b49a214913308ccc1
DimaKalinin222 <dimon163228@yandex.ru>
2025-09-23 19:06:35 +0300

    5.4

---
+++ subject5/main4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main() {
+    std::srand(std::time(nullptr));
+    int n, m;
+    std::cout << "Enter number of rows: ";
+    std::cin >> n;
+    std::cout << "Enter number of columns: ";
+    std::cin >> m;
+
+    if (n <= 0 || m <= 0) {
+        std::cout << "Rows and columns must be positive\n";
+        return 1;
+    }
+
+    int** arr = new int* [n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[m];
+    }
+
+    std::cout << "Array:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            arr[i][j] = std::rand() % 100; 
+            std::cout << arr[i][j] << "\t";
+        }
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
